68dafa0a844b5c2a0bb8830a7c342842f6893c57
SpaceMovie1992 <ahusic@LAPTOP-O8GPUUMU>
2024-06-29 06:25:06 +0200

    added files init args and philos and forks which contain functions to initialize philosophers and forks as well as arguments

--- error_and_msg.c
+++ error_and_msg.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_handle.c                                     :+:      :+:    :+:   */
+/*   error_and_msg.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahusic <ahusic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:43:20 by ahusic            #+#    #+#             */
-/*   Updated: 2024/06/25 19:46:35 by ahusic           ###   ########.fr       */
+/*   Updated: 2024/06/29 05:52:42 by ahusic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,43 @@ int	error_msg(char *msg)
 	write(2, msg, ft_strlen(msg));
 	write(2, "\n", 1);
 	return (1);
+}
+
+
+void	finished(t_simulation *sim, bool all)
+{
+	int	i;
+
+	i = 0;
+	if (all)
+	{
+		while (i < sim->num_philos)
+		{
+			pthread_mutex_destroy(&(sim->forks));
+			i++;
+		}
+		pthread_mutex_destroy(&sim->dead_lock);
+		pthread_mutex_destroy(&sim->print_lock);
+		pthread_mutex_destroy(&sim->eat_lock);
+	}
+	if (sim->forks != NULL)
+	{
+		free(sim->forks);
+		sim->forks = NULL;
+	}
+	if (sim->philo != NULL)
+	{
+		free(sim->philo);
+		sim->philo = NULL;
+	}
+}
+
+void	msg_philos(char *msg, t_philo *philo, int id)
+{
+	if (!philo->sim->dead)
+	{
+		pthread_mutex_lock(&philo->sim->print_lock);
+		printf("%d %d %s\n", get_time() - philo->start_time, id, msg);
+		pthread_mutex_unlock(&philo->sim->print_lock);
+	}
 }
---
+++ init_philos_forks.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_philos_forks.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahusic <ahusic@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 05:49:17 by ahusic            #+#    #+#             */
+/*   Updated: 2024/06/29 06:15:35 by ahusic           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	philo_init(t_simulation *sim)
+{
+	t_philo	*philo;
+	int		i;
+
+	philo = malloc(sizeof(t_philo) * sim->num_philos);
+	if (philo == NULL)
+		return (1);
+	sim->philo = philo;
+	i = 0;
+	while(i < sim->num_philos)
+	{
+		philo[i].sim = sim;
+		philo[i].id = i + 1;
+		philo[i].eating = 0;
+		philo[i].num_meals = 0;
+		philo[i].start_time = get_time();
+		philo[i].last_meal = get_time();
+		philo[i].right_fork = &sim->forks[i];
+		if (i == sim->num_philos - 1)
+			philo[i].left_fork = &sim->forks[0];
+		else
+			philo[i].left_fork = &sim->forks[i + 1];
+		i++;
+	}
+	return (0);
+}
+
+
+int	forks_init(t_simulation *sim)
+{
+	int				i;
+	pthread_mutex_t	*forks;
+
+	i = 0;
+	forks = malloc(sizeof(pthread_mutex_t) * sim->num_philos);
+	if (forks == NULL)
+		return (1);
+	sim->forks = forks;
+	while (i < sim->num_philos)
+	{
+		if (pthread_mutex_init(&forks[i], NULL) != 0)
+		{
+			while (--i >= 0)
+				pthread_mutex_destroy(&forks[i]);
+			free(forks);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
